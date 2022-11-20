a1e3d2074d10513c2a01fb3a8126639830cdfce7
Munir Alsafi <munir.aljaw@gmail.com>
2022-11-20 02:48:16 -0500

    add meal object

---
+++ app/src/main/java/com/example/mealerapp/objects/Meal.java
@@ -0,0 +1,26 @@
+package com.example.mealerapp.objects;
+
+public class Meal {
+
+    private String mealName;
+    private String mealDescription;
+    private float price;
+
+    public Meal(String mealName, String mealDescription, float price) {
+        this.mealName = mealName;
+        this.mealDescription = mealDescription;
+        this.price = price;
+    }
+
+    public String getMealName() {
+        return mealName;
+    }
+
+    public String getMealDescription() {
+        return mealDescription;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+}
